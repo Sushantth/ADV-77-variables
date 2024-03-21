9495ceaaef4c68136674e4a145264ed17b53310c
Sushantth <98395347+Sushantth@users.noreply.github.com>
2024-03-21 17:22:42 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+var first_name = Ranbir;
+var second_name = Kapoor;
+var merged = first_name + second_name ;
+
+function concat() {
+console.log(merged) ;
+}
+
+var a = 10;
+var b = 5;
+var add = a+b;
+
+console.log(add);
